089f5f6e21cfa9978b674dc135cddbc7056eb698
AlbertoYovani <aypp1996@hotmail.com>
2017-03-11 16:57:01 -0600

    Nuevo index
    Se agrega nuevo index_2

---
+++ index_2.php
@@ -0,0 +1,304 @@
+<?php include './include/header.php';?>
+             <!--Content Central -->
+            <div class="section-title-01">
+                <div class="bg_parallax image_04_parallax" style="background: url(img/footer3.jpg);background-size: cover!important;background-position: center"></div>
+                <div class="opacy_bg_02">
+                     <div class="container">
+                         <br><br><br><br>
+                         <img src="img/logo.png" style="width: 210px; height: 210px;">
+                    </div>  
+                </div>  
+            </div><br><br>
+            <section class="content-central">
+                <!-- Shadow Semiboxed -->
+                <div class="semiboxshadow text-center">
+                    <img src="img/img-theme/shp.png" class="img-responsive" alt="">
+                </div>
+                <!-- End Shadow Semiboxed -->
+
+                <!-- End content info - Vacations Destinatios-->
+                <div class="content_info">
+                    <!-- content-boxes-->
+                    <div class="content-boxes">
+                        <!-- Item-boxe-->
+                        <div class="item-boxed">
+                            <div class="image-boxed">
+                                <span class="overlay"></span>
+                                <img src="img/arreglos/img2.png" alt="" style="width: 300px; height: 300px;">
+                                <a href="hotel-detailed.html" class="more-boxe"><i class="fa fa-plus-circle"></i></a>
+                            </div>
+                            <div class="info-boxed boxed-top">
+                                <h3>
+                                    Hot Baloon Special Journey<br>
+                                    <span>DPM, CBE, TAMILNADU</span>
+                                </h3>
+                                <hr class="separator">
+                                <p>The Royal National is in London near Covent Garden.</p>
+                                <ul class="starts">
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star-half-empty"></i></a></li>
+                                </ul>
+                                <div class="content-btn"><a href="hotel-detailed.html" class="btn btn-primary">View Details</a></div>
+                                <div class="price"><span>$</span> 45</div>
+                            </div>
+                        </div>
+                        <!-- End Item-boxe-->
+
+                        <!-- Item-boxe-->
+                        <div class="item-boxed">
+                            <div class="image-boxed">
+                                <span class="overlay"></span>
+                                <img src="img/arreglos/img2.png" alt="" style="width: 300px; height: 300px;">
+                                <a href="hotel-detailed.html" class="more-boxe"><i class="fa fa-plus-circle"></i></a>
+                            </div>
+                            <div class="info-boxed boxed-top">
+                                <h3>
+                                    The Grand Canyons<br>
+                                    <span>PEELAMEDU, CBE, INDIA</span>
+                                </h3>
+                                <hr class="separator">
+                                <p>The Holiday Inn Bilbao is in a prime location next to the Basilica of Begoña.</p>
+                                <ul class="starts">
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star-half-empty"></i></a></li>
+                                </ul>
+                                <div class="content-btn"><a href="hotel-detailed.html" class="btn btn-primary">View Details</a></div>
+                                <div class="price"><span>$</span> 65</div>
+                            </div>
+                        </div>
+                        <!-- End Item-boxe-->
+
+                        <!-- Item-boxe-->
+                        <div class="item-boxed">
+                            <div class="info-boxed boxed-bottom">
+                                <h3>
+                                    Weekdays in Thailand<br>
+                                    <span>PEELAMEDU, CBE, INDIA</span>
+                                </h3>
+                                <hr class="separator">
+                                <p>The spacious rooms have a bathroom with hairdryer, direct dial telephone.</p>
+                                <ul class="starts">
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star-half-empty"></i></a></li>
+                                </ul>
+                                <div class="content-btn"><a href="hotel-detailed.html" class="btn btn-primary">View Details</a></div>
+                                <div class="price"><span>$</span> 30</div>
+                            </div>
+                            <div class="image-boxed image-bottom">
+                                <span class="overlay"></span>
+                                <img src="img/arreglos/img2.png" alt="" style="width: 300px; height: 300px;">
+                                <a href="hotel-detailed.html" class="more-boxe"><i class="fa fa-plus-circle"></i></a>
+                            </div>
+                        </div>
+                        <!-- End Item-boxe-->
+
+                        <!-- Item-boxe-->
+                        <div class="item-boxed">
+                            <div class="info-boxed boxed-bottom">
+                                <h3>
+                                    Peek Mountain View<br>
+                                    <span>LAS VEGAS, NEVADA</span>
+                                </h3>
+                                <hr class="separator">
+                                <p>The Golden Bay, heated and opened in 2003, with a total of 380 rooms.</p>
+                                <ul class="starts">
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-star-half-empty"></i></a></li>
+                                </ul>
+                                <div class="content-btn"><a href="hotel-detailed.html" class="btn btn-primary">View Details</a></div>
+                                <div class="price"><span>$</span> 80</div>
+                            </div>
+                            <div class="image-boxed image-bottom">
+                                <span class="overlay"></span>
+                                <img src="img/arreglos/img2.png" alt="" style="width: 300px; height: 300px;">
+                                <a href="hotel-detailed.html" class="more-boxe"><i class="fa fa-plus-circle"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>   
+                <br>
+                <section class="content_info">
+                    <!-- Info Resalt-->
+                    <div class="padding-bottom">
+                        <!-- Title -->
+                        <div class="container">
+                            <div class="row">
+                                <div class="titles">
+                                    <h2 style="color: #42A9D3">Para toda ocasión</h2>
+                                    <hr class="tall">
+                                </div>                    
+                            </div>
+                        </div>
+                        <div id="boxes-carousel">
+                            <div>
+                                <div class="img-hover">
+                                    <img src="img/slider/boda2.jpg" alt="" class="img-responsive">
+                                    <div class="overlay"><a href="img/slider/boda2.jpg" class="fancybox"><i class="fa fa-plus-circle"></i></a></div>
+                                </div>
+
+                                <div class="info-gallery">
+                                    <h3>
+                                        Bodas
+                                    </h3>
+                                    <hr class="separator">
+                                    <p>Los mejores arreglos frutales para una ocasión unica en la vida, disfrutalo acompañado de estos hermosos arreglos.</p>
+                                    <div class="content-btn"><a href="#" class="btn btn-primary">Ver más</a></div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="img-hover">
+                                    <img src="img/slider/cumple2.jpg" alt="" class="img-responsive">
+                                    <div class="overlay"><a href="img/slider/cumple2.jpg" class="fancybox"><i class="fa fa-plus-circle"></i></a></div>
+                                </div>
+                                <div class="info-gallery">
+                                    <h3>
+                                        Cumpleaños
+                                    </h3>
+                                    <hr class="separator">
+                                    <p>Los mejores arreglos frutales para una ocasión unica en la vida, disfrutalo acompañado de estos hermosos arreglos.</p>
+                                    <div class="content-btn"><a href="#" class="btn btn-primary">Ver más</a></div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="img-hover">
+                                    <img src="img/slider/xvanios2.jpg" alt="" class="img-responsive">
+                                    <div class="overlay"><a href="img/slider/xvanios2.jpg" class="fancybox"><i class="fa fa-plus-circle"></i></a></div>
+                                </div>
+                                <div class="info-gallery">
+                                    <h3>
+                                        XV años
+                                    </h3>
+                                    <hr class="separator">
+                                    <p>Los mejores arreglos frutales para una ocasión unica en la vida, disfrutalo acompañado de estos hermosos arreglos.</p>
+                                    <div class="content-btn"><a href="#" class="btn btn-primary">Ver más</a></div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="img-hover">
+                                    <img src="img/slider/bautizos1.jpg" alt="" class="img-responsive" style="height: 206px !important;">
+                                    <div class="overlay"><a href="img/slider/bautizos1.jpg" class="fancybox"><i class="fa fa-plus-circle"></i></a></div>
+                                </div>
+                                <div class="info-gallery">
+                                    <h3>
+                                        Bautizos
+                                    </h3>
+                                    <hr class="separator">
+                                    <p>Los mejores arreglos frutales para una ocasión unica en la vida, disfrutalo acompañado de estos hermosos arreglos.</p>
+                                    <div class="content-btn"><a href="#" class="btn btn-primary">Ver más</a></div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="img-hover">
+                                    <img src="img/slider/aniversario1.png" alt="" class="img-responsive" style="height: 206px !important;">
+                                    <div class="overlay"><a href="img/slider/aniversario1.png" class="fancybox"><i class="fa fa-plus-circle"></i></a></div>
+                                </div>
+                                <div class="info-gallery">
+                                    <h3>
+                                        Aniversarios
+                                    </h3>
+                                    <hr class="separator">
+                                    <p>Los mejores arreglos frutales para una ocasión unica en la vida, disfrutalo acompañado de estos hermosos arreglos.</p>
+                                    <div class="content-btn"><a href="#" class="btn btn-primary">Ver más</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End boxes-carousel-->
+                    </div>
+                    <!-- End Info Resalt-->
+                </section> 
+                
+                <br><br>
+                <!-- End content info - Services Items-->
+                <div class="content_info">
+                    <div class="bg-dark color-white border-top" style="background-color: #FA5882 !important;">
+                        <div class="container">
+                            <div class="row">
+                                <!-- Info Resalt - Info Services -->
+                                <div class="col-md-4 ">
+                                    <div class="services-lines-info">
+                                        <h2>Bienvenido a Joally Arreglos Frutales</h2>
+                                        <p class="lead">
+                                            Realiza tus pedidos de la manera más fácil.
+                                            <span class="line"></span>
+                                        </p>
+
+                                        <p>Si buscas momentos inolvidables al lado de las personas que mas quieres, te recomendamos un arreglo especial, con colores, sabores y distintas presenataciones que solo Joally Arreglos Frutales te ofrece.</p>
+                                    </div>
+                                </div>   
+                                <!-- End Info Resalt - Info Services --> 
+
+                                <!-- Services Items -->
+                                <div class="col-md-8">
+                                    <ul class="services-lines">
+                                        <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Ocasiones Especiales</h5>
+                                            </div>
+                                        </li>
+                                         <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Momentos inolvidables</h5>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Reuniones unicas</h5>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Dias de amor</h5>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Bellos momentos</h5>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Logros</h5>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Un bello momento</h5>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="item-service-line">
+                                                <img src="img/arreglos/img2.png" style="width: 80px; height: 80px;">
+                                                <h5>Fechas unicas</h5>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div> 
+                                <!-- End Services Items --> 
+                            </div>
+                        </div>
+                    </div>
+                </div>   
+                <!-- End content info - Services Items --> 
+            </section>
+            <!-- End Content Central -->
+      <?php include './include/footer.php';?>
+    <script src="js/Arreglos.js?<?= md5(microtime())?>"></script>
