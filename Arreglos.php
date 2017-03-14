f468247949561d54fe16dd07780b899d05511c79
felipe de jesus <itifjpp@gmail.com>
2017-03-13 21:12:20 -0600

    Cambios

--- Arreglos.php
+++ Arreglos.php
@@ -6,12 +6,9 @@
 <div class="section-title-01">
     <div class="bg_parallax image_04_parallax" style="background: url(img/fondo1.jpg);background-size: cover;background-position: center"></div>
     <div class="opacy_bg_02">
-<<<<<<< HEAD
-=======
 
         <div class="container" style="margin-top: -50px !important">
             <h1><img src="img/logo.png" style="width: 250px; height: 210px;"></h1><br><br><br><br>
-<<<<<<< HEAD
             <div class="crumbs">
                 <ul>
                     <li><a href="index.php">Inicio</a></li>
@@ -19,10 +16,7 @@
                     <li>Arreglos Frutales</li>    
                 </ul>    
             </div>
-=======
->>>>>>> origin/master
         </div>  
->>>>>>> origin/master
     </div>
 </div>  
 <style>
@@ -99,33 +93,110 @@
                 <!-- Left Sidebar-->
                 <div class="col-md-3 ">
                     <div class=" col-filtro padding-top-mini ">
+                        
                         <h4>BUSCAR ARREGLOS</h4>
-                        <input type="text" name="arreglo_nombre" placeholder="Nombre del Arreglo" class="form-control">
+                        <form method="GET" action="Arreglos.php">
+                        <div class="input-group">
+                            <input type="hidden" name="Clasificacion" value="All">
+                            <input type="hidden" name="Tipo" value="Nombre">
+                            <input type="text" class="form-control" placeholder="Buscar Arreglo" name="Query">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="submit">Buscar</button>
+                            </span>
+                        </div>
+                        </form>
                         <br>
                         <h4>FILTRAR ARREGLOS</h4>
-                        <label class="radio inline" > 
-                            <input type="radio" name="arreglo_clasficacion" value="0" data-tipo="General">
-                            <span>Todos los Arreglos </span> 
-                        </label><br>
-                        <label class="radio inline"> 
-                            <input type="radio" name="arreglo_clasficacion" value="1" data-tipo="Clasificacion">
-                            <span>Sin Chocolate </span> 
-                        </label><br>
-                        <label class="radio inline"> 
-                            <input type="radio" name="arreglo_clasficacion" value="2" data-tipo="Clasificacion">
-                            <span>Con Chocolate </span> 
-                        </label><br>
-                        <label class="radio inline"> 
-                            <input type="radio" name="arreglo_clasficacion" value="3" data-tipo="Clasificacion">
-                            <span>Con Extra Chololate </span> 
-                        </label>
+                        <a href="Arreglos.php?Tipo=General&Clasificacion=0&Query=" class="filtro_class">
+                            <label class="radio inline" > 
+                                <input type="radio" name="arreglo_clasficacion" <?=$_GET['Clasificacion']==0 ? 'checked': ''?> value="0" data-tipo="General">
+                                <span>Todos los Arreglos </span> 
+                            </label>
+                        </a><br>
+                        <a href="Arreglos.php?Tipo=Clasificacion&Clasificacion=1&Query=" class="filtro_class">
+                            <label class="radio inline"> 
+                                <input type="radio" name="arreglo_clasficacion" <?=$_GET['Clasificacion']==1 ? 'checked': ''?> value="1" data-tipo="Clasificacion">
+                                <span>Sin Chocolate </span> 
+                            </label>
+                        </a><br>
+                        <a href="Arreglos.php?Tipo=Clasificacion&Clasificacion=2&Query=" class="filtro_class">
+                            <label class="radio inline"> 
+                                <input type="radio" name="arreglo_clasficacion" <?=$_GET['Clasificacion']==2 ? 'checked': ''?> value="2" data-tipo="Clasificacion">
+                                <span>Con Chocolate </span> 
+                            </label>
+                        </a><br>
+                        <a href="Arreglos.php?Tipo=Clasificacion&Clasificacion=3&Query=" class="filtro_class">
+                            <label class="radio inline"> 
+                                <input type="radio" name="arreglo_clasficacion" <?=$_GET['Clasificacion']==3 ? 'checked': ''?> value="3" data-tipo="Clasificacion">
+                                <span>Con Extra Chololate </span> 
+                            </label>
+                        </a>
                     </div>
                 </div>
                 <!-- End Left Sidebar-->
 
                 <div class="col-md-9 col-content" >
                     <br><br>
-                    <div class="row list-view row-list-arreglos " style="height: 750px"></div>
+                    <div class="row list-view row-list-arreglos " >
+                        <?php
+                            if(isset($_GET['Tipo'])){
+                                $Valor=$_GET['Query'];
+                                $FILTRO_TIPO=$_GET['Tipo'];
+                                $Clasificacion=$_GET['Clasificacion'];
+                                if($FILTRO_TIPO=='Nombre'){
+                                    $sql= mysqli_query(ConexionBd(), "SELECT * FROM arreglo, jy_clasificacion, jy_arreglo_clasificacion
+                                                                WHERE
+                                                                jy_arreglo_clasificacion.clasificacion_id=jy_clasificacion.clasificacion_id AND
+                                                                jy_arreglo_clasificacion.arreglo_id=arreglo.id AND arreglo.nombre LIKE '%$Valor%'");
+                                }if($FILTRO_TIPO=='Clasificacion'){
+                                    $sql= mysqli_query(ConexionBd(), "SELECT * FROM arreglo, jy_clasificacion, jy_arreglo_clasificacion
+                                                                WHERE
+                                                                jy_arreglo_clasificacion.clasificacion_id=jy_clasificacion.clasificacion_id AND
+                                                                jy_arreglo_clasificacion.arreglo_id=arreglo.id AND  jy_clasificacion.clasificacion_id=$Clasificacion");
+                                }if($FILTRO_TIPO=='General'){
+                                    $sql= mysqli_query(ConexionBd(), "SELECT * FROM arreglo, jy_clasificacion, jy_arreglo_clasificacion
+                                                                WHERE
+                                                                jy_arreglo_clasificacion.clasificacion_id=jy_clasificacion.clasificacion_id AND
+                                                                jy_arreglo_clasificacion.arreglo_id=arreglo.id ");
+                                }
+                            }else{
+                                $sql= mysqli_query(ConexionBd(), "SELECT * FROM arreglo, jy_clasificacion, jy_arreglo_clasificacion
+                                        WHERE
+                                        jy_arreglo_clasificacion.clasificacion_id=jy_clasificacion.clasificacion_id AND
+                                        jy_arreglo_clasificacion.arreglo_id=arreglo.id LIMIT 20");
+                            }
+                        $col='';
+                        while ($row = mysqli_fetch_array($sql)) {
+                            $Tam= mysqli_query(ConexionBd(), "SELECT * FROM tamanio WHERE tamanio.arreglo_id=".$row['id']);
+                            $TAM_LIST='';
+                            while ($RESUL_TAM = mysqli_fetch_array($Tam)) {
+                                $TAM_LIST.=$RESUL_TAM['tamanio_nombre'].', ';
+                            }
+                            
+                        ?>
+                        <div class="col-md-12">
+                            <div class="img-hover">
+                                <img src="<?=$row['imagen']?>" alt="" class="img-responsive">
+                                <div class="overlay"><a href="<?=$row['imagen']?>" class="fancybox"><i class="fa fa-plus-circle"></i></a></div>
+                            </div>
+
+                            <div class="info-gallery">
+                                <h3 style="text-transform: uppercase">
+                                    <?=$row['nombre']?><br>
+                                    <span style="text-transform: none"><b>Tamaños:</b> <?= trim($TAM_LIST, ', ')?></span>
+                                </h3>
+                                <hr class="separator">
+                                <p style="height: 40px;"><b>Descripción:</b> <?= substr($row['descripcion'], 0,150)?>...</p>
+                                <ul class="starts">
+                                    <li><i class="fa fa-bookmark-o"></i> <?=$row['clasificacion_nombre']?></li>
+                                </ul>
+                                <div class="content-btn">
+                                    <a href="Detalle.php?id=<?=$row['id']?>" class="btn btn-primary">Ver Detalles</a>
+                                </div>
+                            </div>
+                        </div>
+                        <?php }?>
+                    </div>
                     <br><br><br>
                 </div>
             </div>
--- js/Jy_Arreglos.js
+++ js/Jy_Arreglos.js
@@ -6,45 +6,15 @@ $(document).ready(function (e) {
         
     })
     $('input[name=arreglo_clasficacion]').change(function () {
-        window.history.replaceState(null, null, window.location.pathname);
-        BuscarArreglo($(this).attr('data-tipo'),$(this).val());
-        
+       // window.history.replaceState(null, null, window.location.pathname);
     })
-    BuscarArreglo('General','');
-    function BuscarArreglo(tipo_filtro,Valor) {
-        $('.row-list-arreglos').html('<div class="col-md-12" >'+
-                                            '<center>'+
-                                                '<br><br><br><br><br>'+
-                                                '<i class="fa fa-spinner fa-pulse fa-3x" style="color: #88C425 !important"></i><br>'+
-                                                '<h4>Buscando arreglos, espero por favor...</h4>'+
-                                            '</center>'+
-                                        '</div>');
-        $.ajax({
-            url: "controlador/AjaxArreglos.php",
-            type: 'POST',
-            dataType: 'json',
-            data:{
-                tipo_filtro:tipo_filtro,
-                Valor:Valor,
-                accion:'BUSCAR_ARREGLOS'
-            },beforeSend: function (xhr) {
-            },success: function (data, textStatus, jqXHR) {
-                $('.row-list-arreglos').html(data.arreglos);
-                Paginacion()
-            },error: function (e) {
-                console.log(e);
-            }
-        })
-    }
-    $('.col-filtro').css({
-        height:$('.col-content').height()+'px'
+    $('.filtro_class').click(function (e) {
+        e.preventDefault()
+        location.href=$(this).attr('href');;
     })
-    function Paginacion() {
-        $('.row-list-arreglos').paginate({
+    $('.row-list-arreglos').paginate({
                     perPage:                3,      
                     autoScroll:             true,       
                     paginatePosition:       ['bottom'] 
                 });
-    }
-    
 })
